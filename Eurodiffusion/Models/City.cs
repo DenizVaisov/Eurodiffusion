b854f5aa18eb35ae92b4013b0008f79504a238c7
DenizVaisov <deniz.vaisov@gmail.com>
2022-09-09 17:31:44 +0300

    Remove CityCoords and CountryCoords add logic in class City, Country

--- Eurodiffusion/Models/City.cs
+++ Eurodiffusion/Models/City.cs
@@ -93,4 +93,20 @@ namespace Eurodiffusion.Models
             NeighborsCount--;
         }
     }
+
+    /// <summary>
+    /// Координаты для расстановки городов
+    /// </summary>
+    public struct CityCoords
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public CityCoords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
 }
--- Eurodiffusion/Models/Country.cs
+++ Eurodiffusion/Models/Country.cs
@@ -162,4 +162,26 @@ namespace Eurodiffusion.Models
             return false;
         }
     }
+
+    /// <summary>
+    /// Координаты для страны
+    /// </summary>
+    public struct CountryCoords
+    {
+        public int Xl { get; set; }
+
+        public int Yl { get; set; }
+
+        public int Xh { get; set; }
+
+        public int Yh { get; set; }
+
+        public CountryCoords(int xl, int yl, int xh, int yh)
+        {
+            Xl = xl;
+            Yl = yl;
+            Xh = xh;
+            Yh = yh;
+        }
+    }
 }
